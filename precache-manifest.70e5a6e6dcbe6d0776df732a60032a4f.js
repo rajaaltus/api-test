69c687f5c12738536cc7a0c0d90cd64fe9f92eed
Rajas <rajaaltus@gmail.com>
2019-04-23 20:37:02 +0700

    Updates

---
+++ precache-manifest.70e5a6e6dcbe6d0776df732a60032a4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce35fba5601e0c882f0a9358773d94c",
+    "url": "/api-test/index.html"
+  },
+  {
+    "revision": "8879f751ae83ccbafa24",
+    "url": "/api-test/static/css/main.18fea23f.chunk.css"
+  },
+  {
+    "revision": "3f482bcd301556f5c8e5",
+    "url": "/api-test/static/js/2.9085ed92.chunk.js"
+  },
+  {
+    "revision": "8879f751ae83ccbafa24",
+    "url": "/api-test/static/js/main.4d1d989f.chunk.js"
+  },
+  {
+    "revision": "e5d5f66c855a3037c324",
+    "url": "/api-test/static/js/runtime~main.3fa43213.js"
+  }
+]);
