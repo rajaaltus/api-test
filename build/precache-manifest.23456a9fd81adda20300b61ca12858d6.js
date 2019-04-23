7571c0553ce3ca08c69aa9faab1f86bb3b9b00d1
Rajas <rajaaltus@gmail.com>
2019-04-23 19:49:12 +0700

    initial with build

---
+++ build/precache-manifest.23456a9fd81adda20300b61ca12858d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6fa0d51bc052110008012171ef1d8f2",
+    "url": "/mobi-test/index.html"
+  },
+  {
+    "revision": "8879f751ae83ccbafa24",
+    "url": "/mobi-test/static/css/main.18fea23f.chunk.css"
+  },
+  {
+    "revision": "ce7cff1659e20fc454d2",
+    "url": "/mobi-test/static/js/2.ef37d352.chunk.js"
+  },
+  {
+    "revision": "8879f751ae83ccbafa24",
+    "url": "/mobi-test/static/js/main.4d1d989f.chunk.js"
+  },
+  {
+    "revision": "3bd6bb63d7ecbdf9c29e",
+    "url": "/mobi-test/static/js/runtime~main.a2db38cf.js"
+  }
+]);
